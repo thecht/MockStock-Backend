63436534f8d0340f672956cd43d6ff7a1a5f3cac
Trey <thecht182@gmail.com>
2019-02-01 10:17:21 -0500

    initial commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MockStockBackend.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+    }
+}

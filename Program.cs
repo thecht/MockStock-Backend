bca09014b4ca43e1d735903b3d51169f7e64e85f
Jonathan Langston <JTLangston96@gmail.com>
2019-02-05 12:18:30 -0500

    Testing commit

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+//Testing comment
+
 namespace MockStockBackend
 {
     public class Program

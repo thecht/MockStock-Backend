2049813544a2c9a33ae874d6a10f89aaa1a536c4
Sebastian Schwagerl <sbschwag@uncg.edu>
2019-03-27 15:38:39 -0400

    Added Migrations for League

---
+++ Migrations/20190327193804_AddsLeagueProperties.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MockStockBackend.Migrations
+{
+    public partial class AddsLeagueProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LeagueCreationDate",
+                table: "Leagues",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LeagueHost",
+                table: "Leagues",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LeagueName",
+                table: "Leagues",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LeagueCreationDate",
+                table: "Leagues");
+
+            migrationBuilder.DropColumn(
+                name: "LeagueHost",
+                table: "Leagues");
+
+            migrationBuilder.DropColumn(
+                name: "LeagueName",
+                table: "Leagues");
+        }
+    }
+}
